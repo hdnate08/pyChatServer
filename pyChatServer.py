94e1de077a0640b6e39c9f4ae48b654e57277734
nharris <nate.harris60@gmail.com>
2023-07-18 21:05:55 -0500

    Hashing out issues with the client connecting to the server, not yet sure whats wrong.

--- pyChatServer.py
+++ pyChatServer.py
@@ -24,32 +24,33 @@ class PyChatServer:
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
         self.is_running = True
-        # The next two lines specify a list of sockets to be monitored for availability,
-        # and the function, which does the monitoring 'select()'. The two empty arguments
-        # are for 'wlist' and 'xlist', which are lists of sockets that can be checked for
-        # outgoing data, which can be written and exceptional conditions, such as errors,
-        # respectively.
-        sockets_to_check = [self.server_socket]
-        ready_socket = select.select(sockets_to_check, [], [])
 
-        # Only accept if the socket is open, this needed to be done because the thread would
-        # attempt to run accept() on a closed socket and throw an error.
-        while self.is_running and (ready_socket == self.server_socket):
-            client_socket, client_address = self.server_socket.accept()
-            self.to_Log(f"Client connected {client_address[0]}:{client_address[1]}")
+        while self.is_running:
+            sockets_to_check = [self.server_socket]
+            ready_sockets, _, _ = select.select(sockets_to_check, [], [])
 
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
-            self.client_threads.append(client_thread)
-            client_thread.start()
+            for ready_socket in ready_sockets:
+                if ready_socket == self.server_socket:
+                    client_socket, client_address = self.server_socket.accept()
+                    self.to_Log(f"Client connected {client_address[0]}:{client_address[1]}")
+
+                    client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+                    self.client_threads.append(client_thread)
+                    client_thread.start()
 
     # The threading outputs below can be removed once its ensured that threading is being
     # handled properly
-    def handle_client(self):
+    def handle_client(self, client_socket):
         current_thread = threading.current_thread()
         self.to_Log(f"Thread {current_thread.ident} started")
         while self.is_running:
-            time.sleep(5)
-            self.to_Log(f"Thread {current_thread.ident} printing from handle_client()")
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+        client_socket.close()
+        self.to_Log(f"Thread {current_thread.ident} closed")
+
 
     def stop(self):
         self.to_Log(f'Server closed {self.host}:{str(self.port)}')
@@ -57,6 +58,6 @@ class PyChatServer:
             self.is_running = False
             self.server_socket.close()
 
-            for thread in self.client_threads:
-                self.to_Log(f"Thread {thread.ident} joining...")
-                thread.join()
+        for thread in self.client_threads:
+            self.to_Log(f"Thread {thread.ident} joining...")
+            thread.join()
